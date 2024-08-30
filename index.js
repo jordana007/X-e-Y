62ad6b3e7489114f2ff3734dd98c0ab1db2eda88
jordana <133382207+jordana007@users.noreply.github.com>
2024-08-30 00:45:16 +0000

    Initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const { Z_ERRNO} = require("zlib")
+
+
+//crie um peograma que leia dois números x e y indefinidamente até que um deles seja zero. esses pontos devem representar a localização no plano cartesiano e o programa deve imprimir em wual quadrante o ponto se encontra.
+
+while (true) {
+  var x = Number(prompt("Digite o valor de x:"));
+  var y = Number(prompt("Digite o valor de y:"));
+
+
+
+
+
+
+
